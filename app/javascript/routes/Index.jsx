f426f881015582b64f3d5b1a8343013997314171
Mason Cole <mcole18@asu.edu>
2020-10-11 03:44:05 -0700

    update example data viz

--- app/javascript/routes/Index.jsx
+++ app/javascript/routes/Index.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "../components/Home";
+import Demo from "../components/Demo";
 
 export default (
   <Router>
     <Switch>
       <Route path="/" exact component={Home} />
+        <Route path="/demo" exact component={Demo} />
     </Switch>
   </Router>
 );
